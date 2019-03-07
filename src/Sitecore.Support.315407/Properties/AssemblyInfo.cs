2aa54a702c09ad81e82dabf4b57609787b91fb4a
EOP <eop@sitecore.com>
2019-03-07 10:09:06 +0200

    Initial commit

---
+++ src/Sitecore.Support.315407/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.315407")]
+[assembly: AssemblyProduct("Sitecore.Support.315407")]
+[assembly: ComVisible(false)]
